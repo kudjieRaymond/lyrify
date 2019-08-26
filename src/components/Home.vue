d036862c245cc05379826797d551d2c37dba975d
Ray Kudjie <rkudjie@gmail.com>
2019-08-26 21:33:21 +0000

    Refactor Home page to fetch data before navigation
    add Searching  text
    
    Refactor Home page to fetch data before navigation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-			<div class="jumbotron jumbotron-fluid search-cont ">
-				<div class="container">
-					<h2 class="jumbotron-heading text-white mb-5">Welcome to Lyrify, Enter Any Song and we shall get the Lyrics  for you</h2>
-					<form class="form-inline">
-						<div class="form-group col-md-8">
-						<input class="form-control ht w-100" v-model="track" type="search" placeholder="Search Track" aria-label="Search" >
-						</div>
-						<div class="form-group col-md-4">
-							<button class="btn btn-block btn-outline-info ht" @click.prevent="searchTrack()">Search</button>
-						</div>
-					</form>
-				</div>
+		<div v-if="error">
+			{{error}}
+		</div>
+		<div class="jumbotron jumbotron-fluid search-cont ">
+			<div class="container">
+				<h2 class="jumbotron-heading text-white mb-5">Welcome to Lyrify, Enter Any Song and we shall get the Lyrics  for you</h2>
+				<form class="form-inline">
+					<div class="form-group col-md-8">
+					<input class="form-control ht w-100" v-model="track" type="search" placeholder="Search Track" aria-label="Search" >
+					</div>
+					<div class="form-group col-md-4">
+						<button class="btn btn-block btn-outline-info ht" @click.prevent="searchTrack()">Search</button>
+					</div>
+				</form>
 			</div>
+		</div>
+
+		<div v-if="searching" class="row">
+			<h3 class="text-white">Searching ....</h3>
+  	</div>
 		<div class="row">
 			<Track v-for="item in tracks" v-bind:key="item.track_id" v-bind="item.track"></Track>
 		</div>
@@ -23,6 +30,15 @@
 import Track from './track.vue'
 import api from '../api/client'
  
+ const getTracks = (url, callback) =>{
+	api.chartTop({params:{page: 1 , page_size	: 12, country: 'gh' , f_has_lyrics:1, apikey: 'f5ea45c1c8fb098deb912574e46f2c10'}})
+			.then((res)=>{
+				this.tracks = callback(null, res.data.message.body.track_list);
+			})
+			.catch(err=> {
+				callback(err, null); 
+				});
+ }
 export default {
   name: 'app',
   components: {
@@ -31,23 +47,35 @@ export default {
 	data(){
 		return {
 			track:'',
-			tracks: []
+			tracks: [],
+			error: '',
+			searching:false,
 		}
 	},
+	beforeRouteEnter(to, from, next){
+		getTracks(to, (err, data) => {
+			next(vm => vm.setData(err, data))
+		})
+	},
 	created(){
-		api.chartTop({params:{page: 1 , page_size	: 12, country: 'gh' , f_has_lyrics:1, apikey: 'f5ea45c1c8fb098deb912574e46f2c10'}})
-			.then((res)=>{
-				this.tracks = res.data.message.body.track_list;
-			})
-			.catch(err=>console.log(err));
+
 	},
 	methods:{
 		searchTrack(){
+			this.searching =true ;
 			api.find({params: {q_track:this.track ,page_size:10, page:1 ,s_track_rating:'desc', f_has_lyrics:1, apikey: 'f5ea45c1c8fb098deb912574e46f2c10'}})
 					.then((res)=>{
+						this.searching =false;
 						this.tracks = res.data.message.body.track_list;
 					})
 					.catch(err=>{console.log(err)})
+		},
+		setData(err, data){
+			if(err){
+				this.err = err.toString()
+			}else{
+				this.tracks = data
+			}
 		}
 	}
 	
